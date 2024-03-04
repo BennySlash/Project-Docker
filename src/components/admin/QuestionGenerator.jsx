9a32d394042edde959828c6b6dac82eadb4648d9
BennySlash <biniyamtessema29@gmail.com>
2024-03-04 17:47:50 +0300

    question-generator

--- src/components/admin/QuestionGenerator.jsx
+++ src/components/admin/QuestionGenerator.jsx
@@ -5,6 +5,14 @@ const QuestionGenerator = () => {
   const location = useLocation();
   //   console.log(location.state);
   const numberOfQuestions = location.state.examQuestions;
+  const fullQuestion = {
+    question: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+    answer: "",
+  };
   const formsArray = Array.from(
     { length: numberOfQuestions },
     (_, index) => index + 1
@@ -17,30 +25,48 @@ const QuestionGenerator = () => {
   const [optionC, setOptionC] = useState("");
   const [optionD, setOptionD] = useState("");
   const [answer, setAnswer] = useState("");
+  //   console.log({ question, optionA, optionB, optionC, optionD });
+
   const hadleSubmit = (event) => {
-    e.preventDefault();
+    event.preventDefault();
+
+    setQuestionsArray((prevState) => [
+      ...prevState,
+      {
+        question: question,
+        optionA: optionA,
+        optionB: optionB,
+        optionC: optionC,
+        optionD: optionD,
+        answer: answer,
+      },
+    ]);
   };
+  console.log(questionsArray);
+
   const handleChange = (e) => {
-    switch (e.target.id) {
-      case "question":
+    const firstPart = e.target.id.split("-")[0];
+    console.log(firstPart);
+    switch (firstPart) {
+      case "input":
         setQuestion(e.target.value);
         // console.log(question);
         break;
-      case "a":
+      case "optionA":
         setOptionA(e.target.value);
         // console.log(optionA);
         break;
-      case "b":
+      case "optionB":
         setOptionB(e.target.value);
         // console.log(optionB);
 
         break;
-      case "c":
+      case "optionC":
         setOptionC(e.target.value);
         // console.log(optionC);
 
         break;
-      case "d":
+      case "optionD":
         setOptionD(e.target.value);
         // console.log(optionD);
         break;
@@ -60,90 +86,91 @@ const QuestionGenerator = () => {
             <form onSubmit={hadleSubmit}>
               <label
                 className="block mb-2 text-xl font-medium text-gray-900 dark:text-black"
-                htmlFor="question"
+                htmlFor={`input-${item}`}
               >
                 {`Question#${item}`}
               </label>
               <input
                 onChange={handleChange}
-                value={question}
-                id="question"
+                // value={question}
+                id={`input-${item}`}
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
               <label
                 className="block mb-2 text-xl font-medium text-gray-900 dark:text-black"
-                htmlFor="a"
+                htmlFor={`optionA-${item}`}
               >
                 optionA:
               </label>
               <input
                 onChange={handleChange}
-                value={optionA}
-                id="a"
+                // value={optionA}
+                id={`optionA-${item}`}
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
               <label
                 className="block mb-2 text-xl font-medium text-gray-900 dark:text-black"
-                htmlFor="b"
+                htmlFor={`optionB-${item}`}
               >
                 optionB:
               </label>
               <input
                 onChange={handleChange}
-                value={optionB}
-                id="b"
+                // value={optionB}
+                id={`optionB-${item}`}
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
               <label
                 className="block mb-2 text-xl font-medium text-gray-900 dark:text-black"
-                htmlFor="c"
+                htmlFor={`optionC-${item}`}
               >
                 optionC:
               </label>
               <input
                 onChange={handleChange}
-                value={optionC}
-                id="c"
+                // value={optionC}
+                id={`optionC-${item}`}
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
               <label
                 className="block mb-2 text-xl font-medium text-gray-900 dark:text-black"
-                htmlFor="d"
+                htmlFor={`optionD-${item}`}
               >
                 optionD:
               </label>
               <input
                 onChange={handleChange}
-                value={optionD}
-                id="d"
+                // value={optionD}
+                id={`optionD-${item}`}
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
               <label
                 className="block mb-2 text-xl font-medium text-gray-900 dark:text-black"
-                htmlFor="answer"
+                htmlFor={`answer-${item}`}
               >
                 answer:
               </label>
               <input
                 onChange={handleChange}
-                value={answer}
-                id="answer"
+                // value={answer}
+                id={`answer-{item}`}
                 type="text"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
 
               <button
                 type="submit"
-                className="mt-5  font-extrabold focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                className="mt-5 font-extrabold focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
               >
                 submit
               </button>
             </form>
+            <div className="flex justify-center">{`-${item}-`}</div>
           </div>
         );
       })}
